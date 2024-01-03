81455a25294ae19e900413bd4174d65fe23d1d51
rafacoper <rafaelbarbosa.tecnologia@outlook.com>
2024-01-03 11:42:38 -0300

    Working good

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+import { FormsModule } from '@angular/forms';
 
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete
+]
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items : WishItem[] = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Listen Gramatik', true),
+  ]
+
+  listFilter : any = '0';
+
+  newWishText : string = '';
+
   title = 'wishlist';
+
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[this.listFilter])
+  };
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText))
+    this.newWishText = ''
+  }
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }
