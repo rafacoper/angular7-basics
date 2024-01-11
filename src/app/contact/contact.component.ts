43f7ee3eb45ceedd51bee7cb0cb6c327b0712934
rafacoper <rafaelbarbosa.tecnologia@outlook.com>
2024-01-11 19:11:58 -0300

    Contact module running well

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  contactForm = new FormGroup({
+    senderName: new FormControl('', Validators.required),
+    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderMessage: new FormControl('', [Validators.required, Validators.minLength(2), Validators.minLength(10)])
+  });
+
+  submitForm() {
+
+    console.log(this.contactForm.valid);
+    // if (this.senderNameControl.dirty) {
+    //   alert('you changed the name field');
+    // }
+  }
+}
